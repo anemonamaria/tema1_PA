4c09040d23bcf1f2938e595066f38772e9d2b1f2
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2021-04-05 01:22:11 +0300

    Add files via upload

--- crypto.c
+++ crypto.c
@@ -2,38 +2,52 @@
 #include <stdlib.h>
 
 struct pcs{
-	int nrCrypto; // nr actual
-	int cost; //pretul calculatorului
-	int index;
+	int nrCrypto;  // nr actual
+	int cost;  // pretul calculatorului
 };
 
-int main(int argc, char const *argv[]){
-	FILE *f, *g;
-	f = fopen(argv[1], "r");
-	if (f == NULL) {
-		fprintf(stderr, "Eroare: Fisierul nu poate fi deschis %s", "gard.in");
-		return -1;
+int cmpf(const void *x1, const void *x2) {
+	const struct pcs *pa =  x1;
+	const struct pcs *pb =  x2;
+	/*sortez crescator dupa nrCrypto si descrescator dupa cost*/
+	int rez = pa->nrCrypto - pb->nrCrypto;
+	if (rez == 0) {
+		return -(pa->cost - pb->cost);
 	}
-	g = fopen("gard.out", "w");
+	return rez;
+}
 
-	int n, bani, max, min;
+int main(int argc, char const *argv[]) {
+	FILE *f, *g;
+	f = fopen("crypto.in", "r");
+	g = fopen("crypto.out", "w");
+
+	int n, bani, i, min = 999999999;
+	int cost = 0;
 	fscanf(f, "%d %d", &n, &bani);
 	struct pcs *a = malloc(sizeof(struct pcs) * n);
 	for (i = 0; i < n; i++) {
 		fscanf(f, "%d %d", &a[i].nrCrypto, &a[i].cost);
-		a[i].index = i; // de la 0 la 4
+		if (min > a[i].nrCrypto)
+			min = a[i].nrCrypto;
 	}
 	qsort(a, n, sizeof(struct pcs), cmpf);
-	max = a[0].stop;
-	for (i = 1; i < n; i++) {
-		if (a[i].stop <= max) {
-			red++;
-		} else {
-			max = a[i].stop;
+	int aux = min;
+	i = 0;
+	while(cost <= bani) {
+		for(i = 0; i < n; i++) {
+			if (a[i].nrCrypto < aux) {
+				cost = cost + a[i].cost;
+				a[i].nrCrypto ++;
+			} else {
+				break;
+			}
 		}
+		aux++;
 	}
 
-	fprintf(g, "%d", red);
-
+	fprintf(g, "%d\n", aux-2);
+	fclose(f);
+	fclose(g);
 	return 0;
 }
--- stocks.c
+++ stocks.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct firme{
+	int actual;
+	int pierderi;  // actual - minim
+	int profit;  //maxim - actual
+};
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int knapSack(int buget, int pierderi, struct firme *a, int n)
+{
+	//  int W, int wt[], int val[], int n
+	if (n == 0 || buget == 0 || pierderi == 0)
+		return 0;
+	if (a[n-1].pierderi >= pierderi || a[n - 1].actual > buget)
+		return knapSack(buget, pierderi, a, n - 1);
+
+	else
+		return max(
+			a[n - 1].profit
+				+ knapSack(buget - a[n - 1].actual,
+						pierderi - a[n - 1].pierderi, a, n - 1),
+			knapSack(buget, pierderi, a, n - 1));
+}
 
 int main(){
-	printf("Hello world!");
+	FILE *f, *g;
+	f = fopen("stocks.in", "r");
+	g = fopen("stocks.out", "w");
+
+	int n, buget, pierderi, i;
+	fscanf(f, "%d %d %d", &n, &buget, &pierderi);
+	struct firme *a = malloc(sizeof(struct firme) * n);
+	int d, b, c;
+	for (i = 0; i < n; i++) {
+		fscanf(f, "%d %d %d", &a[i].actual, &b, &c);
+		a[i].profit = c - a[i].actual;
+		a[i].pierderi = b - a[i].actual;
+	}
+
+	fprintf(g, "%d", knapSack(buget, pierderi, a, n));
+
+	fclose(f);
+	fclose(g);
+	return 0;
 }
